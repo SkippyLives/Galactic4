bba7fe7ec3c011a76df9263854da9c142192d976
SkippyLives <lrunkle911@gmail.com>
2016-03-28 22:26:58 -0400

    added raw images to project. started on menu system

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour {
+
+    public void StartSinglePlayer()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void StartMultiplayer()
+    {
+        SceneManager.LoadScene("NetworkManager");
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
